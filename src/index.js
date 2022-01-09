7f0e54d5edeb72955749188f67dc422113982b7c
Richa Gohel <richa3.gohel@gmail.com>
2022-01-08 19:09:09 -0500

    Adde navbar and code restructring

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Counter from './components/counter';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
+
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 
